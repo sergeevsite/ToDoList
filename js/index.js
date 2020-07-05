cd60fb15bf8828b7b02c2575627303657cfa7a23
Andrey Sergeev <andreyinoks93@yandex.ru>
2020-07-05 10:57:17 +0700

    lesson22 done

--- js/index.js
+++ js/index.js
@@ -1,81 +1,135 @@
 'use strict';
 
-// Основные переменные
-const todoControl = document.querySelector('.todo-control'),
-      headerInput = document.querySelector('.header-input'),
-      todoList = document.querySelector('.todo-list'),
-      todoCompleted = document.querySelector('.todo-completed');
+class Todo {
 
-// Массив данных из задач
-const todoData = (localStorage.tasks) ? JSON.parse(localStorage.getItem('tasks')) : [];
-
-// Формирование списка задач
-const render = function() {
-
-  // Чистка перед обновлением списка
-  todoList.textContent = '';
-  todoCompleted.textContent = '';
-
-  // Перебор массива с данными задачи
-  todoData.forEach(function(item, i) {
-
-    // Создание верстки под новую задачу
-    const li = document.createElement('li');
-    li.classList.add('todo-item');
-    li.innerHTML = '<span class="text-todo">' + item.value + '</span>' +
-                  '<div class="todo-buttons">' +
-                    '<button class="todo-remove"></button>' +
-                    '<button class="todo-complete"></button>' +
-                  '</div>';
-
-    // Добавление задачи в верстку
-    (item.completed) ? todoCompleted.append(li) : todoList.append(li);
-    
-    // Кнопка для отметки задачи
-    const buttonTodoCompleted = li.querySelector('.todo-complete');
-
-    buttonTodoCompleted.addEventListener('click', function() {
-      item.completed = !item.completed;
-      localStorage.setItem('tasks', JSON.stringify(todoData));
-      render();
-    });
-
-    // Кнопка удаление задачи
-    const buttonTodoRemove = li.querySelector('.todo-remove');
-
-    buttonTodoRemove.addEventListener('click', function() {
-      li.remove();
-      todoData.splice(i, 1);
-      localStorage.setItem('tasks', JSON.stringify(todoData));
-      render();
-    });
-
-  });
-};
-
-
-// Добавление задач в список
-todoControl.addEventListener('submit', function(event) {
-  event.preventDefault();
-
-  // Проверка на пустое поле
-  if(headerInput.value !== '' && headerInput.value.trim() !== '') {
-    // Добавление новой задачи в массив
-    const newTask = {
-      value: headerInput.value,
-      completed: false
-    };
-    todoData.push(newTask);
-  
-    //Запись в LocalStorage
-    localStorage.setItem('tasks', JSON.stringify(todoData));
+  constructor(form, input, todoContainer, todoList, todoComleted) {
+    this.form = document.querySelector(form);
+    this.input = document.querySelector(input);
+    this.todoContainer = document.querySelector(todoContainer);
+    this.todoList = document.querySelector(todoList);
+    this.todoComleted = document.querySelector(todoComleted);
+    this.todoData = new Map(JSON.parse(localStorage.getItem('list')));
   }
 
-  // Чистка поля ввода
-  headerInput.value = '';
+  // Отправка в LocalStorage
+  addToStorage() {
+    localStorage.setItem('list', JSON.stringify([...this.todoData]));
+  }
 
-  render();
-});
+  // Отрисовка
+  render() {
+    this.todoList.textContent = '';
+    this.todoComleted.textContent = '';
+    this.todoData.forEach(this.createItem);
+    this.addToStorage();
+  }
 
-// Подгрузка с LocalStorage
-render();
+  // Создание задачи
+  createItem = (todo) => {
+    const li = document.createElement('li');
+    li.classList.add('todo-item');
+    li.key = todo.key;
+    li.insertAdjacentHTML('beforeend', `
+      <span class="text-todo">${todo.value}</span>
+      <div class="todo-buttons">
+        <button class="todo-remove"></button>
+        <button class="todo-complete"></button>
+      </div>
+    `);
+
+    if(todo.completed) {
+      this.todoComleted.append(li)
+    } else {
+      this.todoList.append(li);
+    }
+
+  }
+
+  // Добавление задачи
+  addTodo(e) {
+    e.preventDefault();
+
+    if(this.input.value.trim()) {
+      const newTodo = {
+        value: this.input.value,
+        completed: false,
+        key: this.generateKey(),
+      };
+      this.todoData.set(newTodo.key, newTodo);
+      this.render();
+      this.error();
+    } else {
+      this.error();
+    }
+    this.input.value = '';
+    
+  }
+  
+  // Ошибка ввода
+  error() {
+    if(this.input.value) {
+      if(this.input.nextElementSibling !== null) {
+        this.input.nextElementSibling.remove();
+      }
+    } else {
+      if(this.input.nextElementSibling === null) {
+        let span = document.createElement('span');
+        span.classList.add('error');
+        span.textContent = 'Поле не может быть пустым';
+        this.input.after(span);
+      }
+    }
+  }
+
+  // Генерация уникального ключа
+  generateKey() {
+    return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+  }
+  
+  // Удалить задачу
+  deleteItem(item) {
+    let li = item.closest('li');
+    this.todoData.forEach((task) => {
+      if(li.key === task.key) {
+        this.todoData.delete(task.key);
+      }
+    });
+    this.render();
+  }
+
+  // Смена статуса задачи
+  completedItem(item) {
+    let li = item.closest('li');
+    this.todoData.forEach((task) => {
+      if(li.key === task.key) {
+        task.completed = !task.completed;
+      }
+    });
+    this.render();
+  }
+
+  // Делегирование
+  handler() {
+    this.todoContainer.addEventListener('click', (e) => {
+      let target = e.target;
+      if(target.classList.contains('todo-remove')) {
+        this.deleteItem(target);
+      }
+      if(target.classList.contains('todo-complete')) {
+        this.completedItem(target);
+      }
+    });
+  }
+
+  // Отправка задач
+  init() {
+    this.form.addEventListener('submit', this.addTodo.bind(this));
+    this.render();
+    this.handler();
+  }
+
+}
+
+const todo = new Todo('.todo-control', '.header-input', '.todo-container', '.todo-list', '.todo-completed');
+
+todo.init();
